39b834d5b2eff3991780ebbd96af698eb45b07f4
SharmilaD <sharmiladaram419@gmail.com>
2025-05-25 13:57:29 +0530

    hello world created

--- 07.special-variables.sh
+++ 07.special-variables.sh
@@ -6,3 +6,6 @@ echo "current script name is: $0"
 echo "present working directory: $PWD"
 echo "User who is running th escript now is: $USER"
 echo "user's home directory is: $HOME"
+echo "PID of the script is: $$"
+sleep 10 &
+echo "PID of the last command ran in back ground is: $! "

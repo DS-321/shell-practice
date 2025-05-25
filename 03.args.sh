bc6905908212f9ed4fe5926820892840670d9bf5
SharmilaD <sharmiladaram419@gmail.com>
2025-05-25 12:24:54 +0530

    hello world created

---
+++ 03.args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hi $person2 how are you?"
+echo "$person2: hi $person1!! i'm good"
+echo "$person1: wtsup $person2, what are your plans?"
+echo "$person2: all good!! nothing so far"

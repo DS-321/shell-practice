c5a254822ecf7f3ecc0037bda358f4d5af6e37cf
SharmilaD <sharmiladaram419@gmail.com>
2025-05-25 12:38:02 +0530

    hello world created

--- 05.datatypes.sh
+++ 05.datatypes.sh
@@ -6,6 +6,6 @@ Number2=200
 Timestamp=$(date)
 
 echo "script executed at $Timestamp"
-SUM=$($Number1+$Number2)
+SUM=$(($Number1+$Number2))
 
-echo "Sum of $Number & $Number2 is $SUM"
+echo "Sum of $Number1 & $Number2 is $SUM"

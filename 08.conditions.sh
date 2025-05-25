ef8d826c7451a0a85f65029a85a332ff235a14dd
SharmilaD <sharmiladaram419@gmail.com>
2025-05-25 15:12:04 +0530

    hello world created

--- 08.conditions.sh
+++ 08.conditions.sh
@@ -4,7 +4,7 @@ Number=$1
 
 if [ $Number -lt 10 ]
 then
-    echo "the number $Number is lethan 10"
+    echo "the number $Number is less than 10"
 else
     echo "the number $Number is not less than 10"
 fi

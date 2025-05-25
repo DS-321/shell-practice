5594c1adec393cde8834ee4484d2a97fb303e07f
SharmilaD <sharmiladaram419@gmail.com>
2025-05-25 12:46:30 +0530

    hello world created

--- 06.array.sh
+++ 06.array.sh
@@ -3,3 +3,7 @@
 Movies=("Pushpa2" "Court" "Bahubali")
 
 echo "First movie is ${Movies[00]}"
+echo "Second movie is ${Movies[01]}"
+echo "Third movie is ${Movies [02]}"
+
+echo "All movies are ${Movies[@]}"

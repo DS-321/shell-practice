83c8e13101291f13dfff77af5229d527bde3cc11
SharmilaD <sharmiladaram419@gmail.com>
2025-05-25 13:53:39 +0530

    hello world created

--- 07.special-variables.sh
+++ 07.special-variables.sh
@@ -2,3 +2,7 @@
 
 echo "all the arguments passed are: $@"
 echo "number of arguments passed are: $#"
+echo "current script name is: $0"
+echo "present working directory: $PWD"
+echo "User who is running th escript now is: $USER"
+echo "user's home directory is: $HOME"

6e0920b075a0dc36aedd27d65083a5476d47eba1
SharmilaD <sharmiladaram419@gmail.com>
2025-05-25 12:21:08 +0530

    hello world created

--- 02.conversation-variables.sh
+++ 02.conversation-variables.sh
@@ -5,5 +5,5 @@ person2=musk
 
 echo "$person1: hi $person2 how are you?"
 echo "$person2: hi $person1!! i'm good"
-echo "$person1: wtsup $person2, what are you plans?"
+echo "$person1: wtsup $person2, what are your plans?"
 echo "$person2: all good!! nothing so far"

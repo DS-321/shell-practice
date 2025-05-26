58c1021ea5573dd1f1097e6877985e9c5631522e
SharmilaD <sharmiladaram419@gmail.com>
2025-05-26 13:32:16 +0530

    hello world created

--- 11.functions-colors.sh
+++ 11.functions-colors.sh
@@ -15,19 +15,19 @@ echo "script started executing at: $(date)" &>>$log_file
 Userid=$(id -u)
 if [ $Userid -ne 0 ]
     then
-    echo -e "$R ERROR: You are not running with root access $N"
+    echo -e "$R ERROR: You are not running with root access $N" | tee -a $log_file
     exit 1
     else
-    echo "You are running with root access"
+    echo "You are running with root access" | tee -a $log_file
 fi
 
 validate(){
 
     if [ $1 -eq 0 ]
     then
-            echo -e "$2 is installed ... $G Successful $N"
+            echo -e "$2 is installed ... $G Successful $N" | tee -a $log_file
             else
-            echo -e "$2 not installed ... $R Failure $N"
+            echo -e "$2 not installed ... $R Failure $N" | tee -a $log_file
             exit 1
     fi        
 }
@@ -35,33 +35,33 @@ validate(){
 dnf list installed mysql &>>$log_file
 if [ $? -ne 0 ]
     then
-    echo "Sql is not installed... going to install now"
+    echo "Sql is not installed... going to install now" | tee -a $log_file
 
         dnf install mysql -y &>>$log_file
         validate $? "mysql"
 
     else
-    echo -e "Sql is already installed... $Y Skipping $N"
+    echo -e "Sql is already installed... $Y Skipping $N" | tee -a $log_file
 fi
 
 dnf list installed nginx &>>$log_file
 if [ $? -ne 0 ]
     then
-    echo "nginx is not installed... going to install now"
+    echo "nginx is not installed... going to install now" | tee -a $log_file
 
         dnf install nginx -y &>>$log_file
         validate $? "nginx"
     else
-    echo -e "nginx is already installed... $Y Skipping $N"
+    echo -e "nginx is already installed... $Y Skipping $N" | tee -a $log_file
 fi
 
 dnf list installed python3 &>>$log_file
 if [ $? -ne 0 ]
     then
-    echo "python3 is not installed... going to install now"
+    echo "python3 is not installed... going to install now" | tee -a $log_file
 
         dnf install python3 -y &>>$log_file
         validate $? "python3"
     else
-    echo -e "python3 is already installed... $Y Skipping $N"
+    echo -e "python3 is already installed... $Y Skipping $N" | tee -a $log_file
 fi

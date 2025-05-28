16048b91ef2b825c0d2d9382cb472d3168f3e26d
SharmilaD <sharmiladaram419@gmail.com>
2025-05-28 17:55:51 +0530

    notes updated

---
+++ 13.delete-old-logfiles.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+Userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+logs_folder="/var/log/roboshop.logs"
+script_name=$(echo $0 | cut -d "." -f1-2)
+log_file="$logs_folder/$script_name.log"
+script_dir=$PWD
+SRC_DIR=/home/ec2-user/app-logs
+
+mkdir -p $logs_folder
+echo "script started executing at: $(date)" | tee -a $log_file
+
+if [ $Userid -ne 0 ]
+    then
+    echo -e "$R ERROR: You are not running with root access $N" | tee -a $log_file
+    exit 1
+    else
+    echo "You are running with root access" | tee -a $log_file
+fi
+validate(){
+
+    if [ $1 -eq 0 ]
+    then
+            echo -e "$2 is installed ... $G Successful $N" | tee -a $log_file
+            else
+            echo -e "$2 not installed ... $R Failure $N" | tee -a $log_file
+            exit 1
+    fi        
+}
+
+    FILES_TO_DELETE=$(find $SRC_DIR -name "*.log" -mtime +14)
+
+    while IFS= read -r filepath
+
+    do
+        echo "deleting file: $filepath" | tee -a $log_file
+        rm -ef $filepath
+
+    done <<< $FILES_TO_DELETE
+
+    echo "Script executed successfully"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $log_file

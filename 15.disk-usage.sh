f2da2d21f9802b9ff455ac26b847914d203e0edb
SharmilaD <sharmiladaram419@gmail.com>
2025-05-30 14:31:19 +0530

    mail session added

--- 15.disk-usage.sh
+++ 15.disk-usage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=2
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-while IFS= read line
+df -hT | grep -v "Filesytem" | while IFS= read line
 do
 
     USAGE=$(echo $line | awk '{print $(NF-1)}' | cut -d "%" -f1)
@@ -13,8 +13,8 @@ do
 
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>"
+        MSG+="High Disk Usage on $PARTITION: $USAGE% <br>"
     fi
-done < $DISK_USAGE
+done 
 
 source ./16.mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "sharmiladaram5@gmail.com" "ALERT-High Disk Usage"

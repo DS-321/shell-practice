ec07287b4ca4c57f41a2123cd795fa97b01c7ee4
SharmilaD <sharmiladaram419@gmail.com>
2025-05-30 14:35:21 +0530

    mail session added

--- 15.disk-usage.sh
+++ 15.disk-usage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=2
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-df -hT | grep -v "Filesystem" | while IFS= read line
+while IFS= read line
 do
 
     USAGE=$(echo $line | awk '{print $(NF-1)}' | cut -d "%" -f1)
@@ -15,6 +15,6 @@ do
     then
         MSG+="High Disk Usage on $PARTITION: $USAGE% <br>"
     fi
-done 
+done <<< $DISK_USAGE
 
 source ./16.mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "sharmiladaram5@gmail.com" "ALERT-High Disk Usage"
--- 16.mail.sh
+++ 16.mail.sh
@@ -11,9 +11,9 @@ SUBJECT=$6
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$FINAL_MSG/g" template.html) 
 
 {
-echo "To: sharmiladaram5@gmail.com"
-echo "Subject: ALERT-High Disk Usage"
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
 echo "$FINAL_BODY"
-} | msmtp "sharmiladaram5@gmail.com"
+} | msmtp "$TO_ADDRESS"

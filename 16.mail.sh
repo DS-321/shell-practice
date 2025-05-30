c4b9a703e1ce33b6f6cc46b334045a8e395a1a77
SharmilaD <sharmiladaram419@gmail.com>
2025-05-30 14:16:32 +0530

    mail session added

--- 16.mail.sh
+++ 16.mail.sh
@@ -11,9 +11,9 @@ SUBJECT=$6
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$FINAL_MSG/g" template.html) 
 
 {
-echo "To: $TO_ADDRESS"
-echo "Subject: $SUBJECT"
+echo "To: sharmiladaram5@gmail.com"
+echo "Subject: ALERT-High Disk Usage"
 echo "Content-Type: text/html"
 echo ""
 echo "$FINAL_BODY"
-} | msmtp "$TO_ADDRESS"
+} | msmtp "sharmiladaram5@gmail.com"
